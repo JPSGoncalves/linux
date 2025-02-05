8bf00740f043a92b153e9d25a05a8acb44736f22
Siddharth Vadapalli <s-vadapalli@ti.com>
2025-02-05 17:48:01 +0530

    dmaengine: ti: k3-udma: Enable second resource range for BCDMA and PKTDMA
    The SoC DMA resources for UDMA, BCDMA and PKTDMA can be described via a
    combination of up to two resource ranges. The first resource range handles
    the default partitioning wherein all resources belonging to that range are
    allocated to a single entity and form a continuous range. For use-cases
    where the resources are shared across multiple entities and require to be
    described via discontinuous ranges, a second resource range is required.
    
    Currently, udma_setup_resources() supports handling resources that belong
    to the second range. Extend bcdma_setup_resources() and
    pktdma_setup_resources() to support the same.
    
    This change resolves the below CPSW probe errors in Linux, which were causing
    Ethernet functionality issues on the Toradex Verdin AM62P. The issue occurred
    after updating the latest U-Boot configuration files (tifs-rm-cfg, rm-cfg) to
    be compatible with TI Linux Firmware version 11.0.6 (Fancy Rat).
    
    am65-cpsw-nuss 8000000.ethernet: initializing am65 cpsw nuss version
    	0x6BA01903, cpsw version 0x6BA81903 Ports: 3 quirks:00000006
    am65-cpsw-nuss 8000000.ethernet: initialized cpsw ale version 1.5
    am65-cpsw-nuss 8000000.ethernet: ALE Table size 512
    am65-cpsw-nuss 8000000.ethernet: CPTS ver 0x4e8a010d, freq:500000000,
    	add_val:1 pps:0
    am65-cpsw-nuss 8000000.ethernet: Failed to get tx dma irq -6
    
    Upstream-Status: Submitted [https://patch.msgid.link/20250205121805.316792-1-s-vadapalli@ti.com]
    Signed-off-by: Siddharth Vadapalli <s-vadapalli@ti.com>
    Acked-by: Peter Ujfalusi <peter.ujfalusi@gmail.com>
    Signed-off-by: Parth Pancholi <parth.pancholi@toradex.com>

--- drivers/dma/ti/k3-udma.c
+++ drivers/dma/ti/k3-udma.c
@@ -4896,6 +4896,12 @@ static int bcdma_setup_resources(struct udma_dev *ud)
 				irq_res.desc[i].start = rm_res->desc[i].start +
 							oes->bcdma_bchan_ring;
 				irq_res.desc[i].num = rm_res->desc[i].num;
+
+				if (rm_res->desc[i].num_sec) {
+					irq_res.desc[i].start_sec = rm_res->desc[i].start_sec +
+									oes->bcdma_bchan_ring;
+					irq_res.desc[i].num_sec = rm_res->desc[i].num_sec;
+				}
 			}
 		}
 	} else {
@@ -4919,6 +4925,15 @@ static int bcdma_setup_resources(struct udma_dev *ud)
 				irq_res.desc[i + 1].start = rm_res->desc[j].start +
 							oes->bcdma_tchan_ring;
 				irq_res.desc[i + 1].num = rm_res->desc[j].num;
+
+				if (rm_res->desc[j].num_sec) {
+					irq_res.desc[i].start_sec = rm_res->desc[j].start_sec +
+									oes->bcdma_tchan_data;
+					irq_res.desc[i].num_sec = rm_res->desc[j].num_sec;
+					irq_res.desc[i + 1].start_sec = rm_res->desc[j].start_sec +
+									oes->bcdma_tchan_ring;
+					irq_res.desc[i + 1].num_sec = rm_res->desc[j].num_sec;
+				}
 			}
 		}
 	}
@@ -4939,6 +4954,15 @@ static int bcdma_setup_resources(struct udma_dev *ud)
 				irq_res.desc[i + 1].start = rm_res->desc[j].start +
 							oes->bcdma_rchan_ring;
 				irq_res.desc[i + 1].num = rm_res->desc[j].num;
+
+				if (rm_res->desc[j].num_sec) {
+					irq_res.desc[i].start_sec = rm_res->desc[j].start_sec +
+									oes->bcdma_rchan_data;
+					irq_res.desc[i].num_sec = rm_res->desc[j].num_sec;
+					irq_res.desc[i + 1].start_sec = rm_res->desc[j].start_sec +
+									oes->bcdma_rchan_ring;
+					irq_res.desc[i + 1].num_sec = rm_res->desc[j].num_sec;
+				}
 			}
 		}
 	}
@@ -5073,6 +5097,12 @@ static int pktdma_setup_resources(struct udma_dev *ud)
 			irq_res.desc[i].start = rm_res->desc[i].start +
 						oes->pktdma_tchan_flow;
 			irq_res.desc[i].num = rm_res->desc[i].num;
+
+			if (rm_res->desc[i].num_sec) {
+				irq_res.desc[i].start_sec = rm_res->desc[i].start_sec +
+								oes->pktdma_tchan_flow;
+				irq_res.desc[i].num_sec = rm_res->desc[i].num_sec;
+			}
 		}
 	}
 	rm_res = tisci_rm->rm_ranges[RM_RANGE_RFLOW];
@@ -5084,6 +5114,12 @@ static int pktdma_setup_resources(struct udma_dev *ud)
 			irq_res.desc[i].start = rm_res->desc[j].start +
 						oes->pktdma_rchan_flow;
 			irq_res.desc[i].num = rm_res->desc[j].num;
+
+			if (rm_res->desc[j].num_sec) {
+				irq_res.desc[i].start_sec = rm_res->desc[j].start_sec +
+								oes->pktdma_rchan_flow;
+				irq_res.desc[i].num_sec = rm_res->desc[j].num_sec;
+			}
 		}
 	}
 	ret = ti_sci_inta_msi_domain_alloc_irqs(ud->dev, &irq_res);
